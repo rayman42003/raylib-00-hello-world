52dd967c9ba0c0ead188807451442caf69457ab9
Richmond Chang <changrichmond@gmail.com>
2021-05-23 22:34:01 -0700

    Draw some eyes on the screen

--- main.c
+++ main.c
@@ -1,80 +1,5 @@
 #include "raylib.h"
-#include "raymath.h"
-#include <stdio.h>
-#include "math.h"
-
-typedef struct IntVector2 {
-    int x;
-    int y;
-} IntVector2;
-
-typedef struct Grid {
-    int row;
-    int col;
-    Vector2 startingPos;
-    Vector2 cellDimensions;
-    char** values;
-} Grid;
-
-bool isValidGridCell(Grid grid, IntVector2 cell) {
-    return cell.x >= 0 && cell.x < grid.col &&
-                cell.y >= 0 && cell.y < grid.row;
-}
-
-Vector2 getGridCellCenter(Grid grid, IntVector2 cell) {
-    if (!isValidGridCell(grid, cell)) {
-        return (Vector2) { .x = -1, .y = -1 };
-    }
-    return (Vector2) { .x = (cell.x + 0.5f) * grid.cellDimensions.x + grid.startingPos.x, 
-        .y = (cell.y + 0.5f) * grid.cellDimensions.y + grid.startingPos.y };
-}
-
-void drawGrid(Grid grid) {
-    int row = grid.row;
-    int col = grid.col;
-    Vector2 startingPos = grid.startingPos;
-    Vector2 cellDimensions = grid.cellDimensions;
-
-    float endX = startingPos.x + cellDimensions.x * col;
-    for (int i = 0; i <= row; i++) {
-        float currY = startingPos.y + cellDimensions.y * i; 
-        DrawLineV((Vector2){ .x = startingPos.x, .y = currY }, 
-            (Vector2){ .x = endX, .y = currY }, LIGHTGRAY);
-    }
-
-    float endY = startingPos.y + cellDimensions.y * row;
-    for (int i = 0; i <= col; i++) {
-        float currX = startingPos.x + cellDimensions.x * i;
-        DrawLineV((Vector2){ .x = currX, .y = startingPos.y },
-            (Vector2){ .x = currX, .y = endY }, LIGHTGRAY);
-    }
-
-    for (int i = 0; i < col; i++) {
-        for (int j = 0; j < row; j++) {
-            char cellValue = grid.values[i][j];
-            if (cellValue != ' ') {
-                Vector2 cellCenter = getGridCellCenter(grid, (IntVector2) { .x = i, .y = j });
-                Vector2 textStart = Vector2Subtract(cellCenter, Vector2Scale(grid.cellDimensions, 0.25f));
-                int fontSize = (int) (fmax(grid.cellDimensions.x, grid.cellDimensions.y) / 1.75);
-                DrawText(&cellValue, textStart.x, textStart.y, fontSize, BLACK);
-            }
-        }
-    }
-}
-
-IntVector2 getGridCell(Grid grid, Vector2 pos) {
-    Vector2 originPos = Vector2Subtract(pos, grid.startingPos);
-    float originPosX = originPos.x;
-    float originPosY = originPos.y;
-    float maxX = grid.cellDimensions.x * grid.col;
-    float maxY = grid.cellDimensions.y * grid.row;
-    if (originPosX < 0 || originPosX > maxX || originPosY < 0 || originPosY > maxY) {
-        return (IntVector2) { .x = -1, .y = -1 };
-    }
-    int cellX = (int) (originPosX / grid.cellDimensions.x);
-    int cellY = (int) (originPosY / grid.cellDimensions.y);
-    return (IntVector2) { .x = cellX, .y = cellY };
-}
+#include "stdio.h"
 
 int main() 
 {
@@ -87,60 +12,35 @@ int main()
     InitWindow(screenWidth, screenHeight, "raylib");
 
     SetTargetFPS(60); 
-    Grid grid = (Grid) {
-        .row = 16,
-        .col = 24,
-        .startingPos = (Vector2)  { .x = 100, .y = 100 },
-        .cellDimensions = (Vector2) { .x = 25, .y = 25 }
-    };
-    char** gridValues = malloc(grid.col * sizeof(char*));
-    for (int i = 0; i < grid.col; i++) {
-        gridValues[i] = malloc(grid.row * sizeof(char));
-        for (int j = 0; j < grid.row; j ++) {
-            gridValues[i][j] = ' ';
-        }
-    }
-    grid.values = gridValues;
 
-    bool player1Turn = true;
+    Vector2 leftEye = (Vector2) { .x = 250, .y = 300 };
+    Vector2 leftPupil = leftEye;
 
-    IntVector2 clickedCell = (IntVector2) { .x = -1, .y = -1};
+    Vector2 rightEye = (Vector2) { .x = 550, .y = 300 };
+    Vector2 rightPupil = rightEye;
+
+    int eyeSize = 120;
+    int pupilSize = 40;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            clickedCell = getGridCell(grid, GetMousePosition());
-            if (isValidGridCell(grid, clickedCell)) {
-                if (grid.values[clickedCell.x][clickedCell.y] == ' ') {
-                    if (player1Turn) {
-                        grid.values[clickedCell.x][clickedCell.y] = 'x';
-                    } else {
-                        grid.values[clickedCell.x][clickedCell.y] = 'o';
-                    }
-                    player1Turn = !player1Turn;
-                }
-            }
-        }
-        char* clickedCellMsg[80];
-        sprintf(clickedCellMsg, "Cell clicked: [%d, %d]", clickedCell.x, clickedCell.y);
-        DrawText(clickedCellMsg, 10, 30, 20, ORANGE);
+        
+        Vector2 mousePos =  GetMousePosition();
+        char* mousePosText[80];
+        sprintf(mousePosText, "Mouse pos: [%.2f, %.2f]", mousePos.x, mousePos.y);
+        DrawText(mousePosText, 10, 30, 20, ORANGE);
 
-        Vector2 boxCenter = getGridCellCenter(grid, clickedCell);
-        char* boxCenterMsg[80];
-        sprintf(boxCenterMsg, "Box center pos: [%0.2f, %0.2f]", boxCenter.x, boxCenter.y);
-        DrawText(boxCenterMsg, 10, 50, 20, ORANGE);
+        DrawCircle(leftEye.x, leftEye.y, eyeSize, LIGHTGRAY);
+        DrawCircle(leftPupil.x, leftPupil.y, pupilSize, BLACK);
 
-        drawGrid(grid);
-
+        DrawCircle(rightEye.x, rightEye.y, eyeSize, LIGHTGRAY);
+        DrawCircle(rightPupil.x, rightPupil.y, pupilSize, BLACK);
+        
         DrawFPS(10, 10);
         EndDrawing();
     }
-
-    for (int i = 0; i < grid.col; i++) {
-        free(grid.values[i]);
-    }
-    free(grid.values);
 
     CloseWindow();
     return 0;
